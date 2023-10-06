788c484ae28ee829657fc4c6ed7cb85b405e3ede
axibedeoan <axi.bedeoan@gmail.com>
2023-10-06 12:14:21 +0300

    fix contact left margin

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,7 +4,7 @@ export const Contact = () => {
   return (
     <div className="container contact">
       <h1 className="title">Contact</h1>
-      <ul>
+      <ul style={{ marginLeft: 20 }}>
         <li>Phone number: +40 754 261 270</li>
         <li>Email: horeadanci@yahoo.com</li>
         <li>LinkedIn: https://www.linkedin.com/in/danci-horea-488527239</li>
