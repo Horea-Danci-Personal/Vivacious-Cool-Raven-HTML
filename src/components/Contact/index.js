8c1a142e34492076f36f2b4d0ccc03ee8aa0cb5b
Horea Danci <horeadanci@yahoo.com>
2023-10-29 17:10:11 +0200

    Update phone number

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -11,7 +11,7 @@ export const Contact = () => {
       </h1>
       <ul style={{ marginLeft: 20 }}>
         <li style={{ marginBottom: 10 }}>
-          <a href="tel:123-456-7890">Phone number: +40 754 261 270</a>
+          <a href="tel:+40 754 261 270">Phone number: +40 754 261 270</a>
         </li>
         <li style={{ marginBottom: 10 }}>
           <a href="mailto:horeadanci@yahoo.com">
